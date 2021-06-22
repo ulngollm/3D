57382c81201a8d8e82581734ef9359078807254d
ulngollm <ulngollm@gmail.com>
2021-06-22 14:50:27 +0300

    add light and material

--- js/script.js
+++ js/script.js
@@ -12,12 +12,19 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 
+//light
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
 
 //объекты сцены
 let geometry = new THREE.SphereGeometry(150, 30, 9);
-let material = new THREE.MeshBasicMaterial({
+let material = new THREE.MeshPhongMaterial({
     color: 0xffff00,
-    wireframe: true,
+    // wireframe: true,
+    // side: THREE.DoubleSide
 });
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
