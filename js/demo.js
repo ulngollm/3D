05916d4aa69d3e8aa5b32c2fcd43777a4d3a4b43
ulngollm <ulngollm@gmail.com>
2021-06-23 00:51:25 +0300

    update demo js

--- js/demo.js
+++ js/demo.js
@@ -1,5 +1,7 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
+
+//размеры холста
 const canvas = document.querySelector('canvas');
 const height = window.innerHeight;
 const width = window.innerWidth;
@@ -14,13 +16,13 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 scene.background = new THREE.Color(0xffffff);
+
 //controls
 const controls = new OrbitControls(camera, canvas);
 controls.enablePan = false;
 controls.maxPolarAngle = Math.PI;
 controls.enableDamping = true;
 controls.dampingFactor = 0.1;
-// controls.autoRotate = true;
 
 //light
 const color = 0xFFFFFF;
@@ -30,19 +32,12 @@ light.position.set(-1, 2, 4);
 scene.add(light);
 
 //объекты сцены
-let geometry = new THREE.SphereGeometry(20, 5, 9);
-let material = new THREE.MeshPhongMaterial({
-    color: 0xd300ff,
-});
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
-sphere.position.x = -400;
-sphere.visible = true;
+const geometry = new THREE.TorusKnotGeometry(80, 30, 50, 15,4);
+const material = new THREE.MeshPhongMaterial({color: 0xd300ff});
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
 
-const cubeGeometry = new THREE.TorusKnotGeometry(80, 30, 50, 15,4);
-const cube = new THREE.Mesh(cubeGeometry, material);
-scene.add(cube);
-
+//settings gui
 let params = {
     rotationX:0,
     rotationY:0,
@@ -61,12 +56,12 @@ gui.add(params, 'positionY').min(-5).max(5).step(0.1);
 gui.add(params, 'positionZ').min(-5).max(5).step(0.1);
 
 function animateObject(time){
-    cube.rotation.x += params.rotationX;
-    cube.rotation.y += params.rotationY;
-    cube.rotation.z += params.rotationZ;
-    cube.position.x += params.positionX;
-    cube.position.y += params.positionY;
-    cube.position.z += params.positionZ;
+    torus.rotation.x += params.rotationX;
+    torus.rotation.y += params.rotationY;
+    torus.rotation.z += params.rotationZ;
+    torus.position.x += params.positionX;
+    torus.position.y += params.positionY;
+    torus.position.z += params.positionZ;
 
     renderer.render(scene, camera);
     controls.update();
