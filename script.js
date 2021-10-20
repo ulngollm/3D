d4f5c1882def38d6cdcf5a5201b660e8d53c5b5b
ulngollm <ul@noknok.ru>
2021-10-20 19:13:34 +0300

    add hover gsap animation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js";
+import gsap from "https://unpkg.com/gsap@3.7.1/gsap-core.js";
 
 const canvas = document.querySelector("canvas");
 const height = window.innerHeight;
@@ -14,16 +15,9 @@ const camera = new THREE.PerspectiveCamera(15, width / height, 10, 2000);
 camera.position.set(0, 0, 50);
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
-    antialias: true
+    antialias: true,
 });
 scene.background = new THREE.Color(0xffffff);
-
-//controls
-const controls = new OrbitControls(camera, canvas);
-controls.enablePan = false;
-controls.maxPolarAngle = Math.PI;
-controls.enableDamping = true;
-controls.dampingFactor = 0.1;
 
 //light
 {
@@ -76,7 +70,11 @@ loader.load("../assets/model/potato.gltf", (gltf) => {
 
 loader.load("../assets/model/pizza_round.gltf", (gltf) => {
     let potato = gltf.scene.children[0];
+    // gltf.scene.traverse(function (child) {
+    //     child.material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+    // });
     scene.add(potato);
+    potato.material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
     potato.position.set(3, 2, 0);
 });
 
@@ -98,7 +96,6 @@ loader.load("../assets/model/combo.gltf", (gltf) => {
     potato.position.set(5, 0, 0);
 });
 
-
 function render() {
     // controls.update();
     renderer.render(scene, camera);
@@ -114,22 +111,33 @@ let draggable = THREE.Object3D;
 window.addEventListener("mousemove", (event) => {
     clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    camera.position.x = clickMouse.x;
-    camera.position.y = clickMouse.y;
+    // camera.position.x = clickMouse.x;
+    // camera.position.y = clickMouse.y;
     raycaster.setFromCamera(clickMouse, camera);
     const found = raycaster.intersectObjects(scene.children, false);
     // console.log(found);
     if (found.length > 0) {
-    //     draggable = found[0].object;
-    //     console.log(`found draggable ${draggable.userData.name}`);
-    //     // draggable.rotation.y = Math.PI/10 * Math.sign(event.movementX);
-    //     // draggable.rotation.x = Math.PI/10 * Math.sign(event.movementY);
-    //     camera.position.x = draggable.position.x;
-    //     camera.position.y = draggable.position.y;
-        camera.position.z = 40;
-
+        draggable = found[0].object;
+        //     console.log(`found draggable ${draggable.userData.name}`);
+        //     // draggable.rotation.y = Math.PI/10 * Math.sign(event.movementX);
+        //     // draggable.rotation.x = Math.PI/10 * Math.sign(event.movementY);
+        //     camera.position.x = draggable.position.x;
+        //     camera.position.y = draggable.position.y;
+        // camera.position.z = 40;
+        if (!draggable.userData.busy) {
+            gsap.to(draggable.rotation, {
+                y: "+=" + (Math.PI / 180) * event.movementX,
+                x: "+=" + (Math.PI / 180) * event.movementY,
+                onStart: function () {
+                    draggable.userData.busy = true;
+                },
+                onComplete: function () {
+                    gsap.to(this._targets, { y: 0, x: 0 });
+                    draggable.userData.busy = false;
+                },
+            });
+        }
     }
-    else camera.position.z = 50;
 
     renderer.render(scene, camera);
 });
