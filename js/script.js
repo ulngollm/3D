eae7285e898ea31c04f02513b5bdd0de543a7fb4
ulngollm <ulngollm@gmail.com>
2021-11-01 15:07:21 +0300

    remove animation, update light and controls

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 import * as THREE from "./three.module.js";
 import {
-    OrbitControls
-} from "./OrbitControls.js";
-import {
-    GLTFLoader
+	GLTFLoader
 } from "./GLTFLoader.js";
-import gsap from "./gsap-core.js";
+import {
+	OrbitControls
+} from "./OrbitControls.js";
 
 const canvas = document.querySelector("canvas");
 const height = window.innerHeight;
@@ -17,120 +16,82 @@ canvas.setAttribute("height", window.innerHeight);
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(15, width / height, 10, 2000);
 camera.position.set(10, 4, 10);
-// const ch = new THREE.CameraHelper(camera);
-// scene.add(ch);
+
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true,
-    alpha: true
+	canvas: canvas,
+	antialias: true,
+	alpha: true
 });
+
 scene.background = new THREE.Color('black');
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
+
 
 //axes
 const axesHelper = new THREE.AxesHelper(10);
 // scene.add(axesHelper);
 
-//controls
-const controls = new OrbitControls(camera, canvas);
-// controls.enablePan = false;
-controls.target.set(0, 0, 0);
-// controls.maxPolarAngle = Math.PI / 6;
-controls.maxAzimuthAngle = Math.PI / 2;
-controls.minAzimuthAngle = Math.PI / 2;
-// controls.enableDamping = true;
-// controls.dampingFactor = 0.1;
-controls.update();
-//light
-// {
-//     //тестовый свет
-//     const color = 0xffffff;
-//     const intensity = 0.8;
-//     const light = new THREE.DirectionalLight(color, intensity);
-//     light.position.set(10, 10, 10);
-//     scene.add(light);
-//     light.castShadow = true;
-//     light.shadow.bias = -0.0001;
-//     light.shadow.mapSize.width = 1024 * 1024
-//     light.shadow.mapSize.height = 1024 * 1024
-// }
-const SHADOW_MAP_SIZE = 1024*1024;
-{
-    //https://redstapler.co/threejs-realistic-light-shadow-tutorial/
-    //https://threejs.org/docs/?q=LightShadow#api/en/lights/shadows/LightShadow
-    const color = 0xffffff;
-    const intensity = 1;
-    const light = new THREE.SpotLight(color, intensity);
-    light.position.set(0, 10, -10);
-    scene.add(light);
 
-    light.castShadow = true;
-    light.shadow.bias = -0.0001;
-    light.shadow.mapSize.width = SHADOW_MAP_SIZE
-    light.shadow.mapSize.height = SHADOW_MAP_SIZE
-	light.shadowDarkness = 0.5;
-	const cameraHelper = new THREE.CameraHelper(light.shadow.camera);
-    // scene.add(cameraHelper);
-} {
-    const color = 0xffffff;
-    const intensity = 0.5;
-    const light = new THREE.SpotLight(color, intensity);
-    light.position.set(0, 5, 0);
-    scene.add(light);
-	light.shadowDarkness = 0.1;
-    light.castShadow = true;
-    light.shadow.bias = -0.0001;
-    light.shadow.mapSize.width = SHADOW_MAP_SIZE
-    light.shadow.mapSize.height = SHADOW_MAP_SIZE
-	const cameraHelper = new THREE.CameraHelper(light.shadow.camera);
-    // scene.add(cameraHelper);
-	// light.shadowDarkness = 0.2;
-}
-renderer.shadowMap.enabled = true;
-// renderer.shadowMap.type = THREE.PCFSoftShadowMap
-// renderer.shadowMap.type = THREE.BasicShadowMap
-// renderer.shadowMap.type = THREE.PCFShadowMap
-// renderer.shadowMap.type = THREE.VSMShadowMap
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.dampingFactor = 0.1;
+controls.update();
+
+
+const lightPosition1 = [0, 5, 0];
+const lightPosition2 = [0, 10, -10];
+const color = 0xffffff;
+const intensity = 0.5;
+[lightPosition1, lightPosition2].forEach((position) => {
+	const light = new THREE.SpotLight(color, intensity);
+	light.position.set(...position);
+	console.log(scene);
+	scene.add(light);
+
+	light.castShadow = true;
+	const SHADOW_MAP_SIZE = 512 * 1024;
+	light.shadow.mapSize.width = SHADOW_MAP_SIZE;
+	light.shadow.mapSize.height = SHADOW_MAP_SIZE;
+	light.shadow.bias = -0.0001;
+})
 
 
 const loader = new GLTFLoader();
 loader.load("../assets/model/cupcake.gltf", (gltf) => {
-    let desert = gltf.scene;
-    desert.traverse((child) => {
-        if (child.isMesh) {
-            if (child.name.includes('oreo')) {
-                // https://threejs.org/docs/#api/en/loaders/TextureLoader
-                const textureLoader = new THREE.TextureLoader();
-                textureLoader.load(
-                    '../assets/model/Oreo.jpg',
-                    function (texture) {
-                        const material = new THREE
-                            .MeshLambertMaterial({
-                                map: texture
-                            });
-                        texture.flipY = false;
-                        child.material = material;
-                    });
-            }
-            if (!child.name.includes('Cylinder')) {
-                child.castShadow = true;
-                child.receiveShadow = true;
-            }
-        }
+	let desert = gltf.scene;
+	desert.traverse((child) => {
+		if (child.isMesh) {
+			if (child.name.includes('oreo')) {
+				// https://threejs.org/docs/#api/en/loaders/TextureLoader
+				const textureLoader = new THREE.TextureLoader();
+				textureLoader.load(
+					'../assets/model/Oreo.jpg',
+					function (texture) {
+						const material = new THREE
+							.MeshLambertMaterial({
+								map: texture
+							});
+						texture.flipY = false;
+						child.material = material;
+					});
+			}
+			if (!child.name.includes('Cylinder')) {
+				child.castShadow = true;
+				child.receiveShadow = true;
+			}
+		}
 
-    })
-    scene.add(desert);
-    desert.scale.set(3, 3, 3);
-
-    gsap.to(desert.rotation, {
-        y: Math.PI*2,
-        duration: 20,
-        ease: "linear",
-        repeat: -1
-    })
+	})
+	scene.add(desert);
+	desert.scale.set(3, 3, 3);
 });
 
 function render() {
-    renderer.render(scene, camera);
-    requestAnimationFrame(render);
+	controls.update();
+	renderer.render(scene, camera);
+	requestAnimationFrame(render);
+
 }
 requestAnimationFrame(render);
